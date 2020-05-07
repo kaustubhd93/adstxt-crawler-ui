110b421ec79db358b4b63edd8358543ca6f3cfa4
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-05-07 18:23:19 +0530

    Researched on Celery, tried to integrate it with spiders. Status of tasks still remains a problem.

--- adstxt/run_spider.py
+++
@@ -1,7 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-process = CrawlerProcess(get_project_settings())
-
-process.crawl('adstxt', fileName="/home/kaustubh/Workspace/adstxt-crawler-ui/docs/sample/sample4")
-process.start()
