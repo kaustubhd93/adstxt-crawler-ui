eb2443ad241f49b461b5a0e34a6441253613e36f
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-05-24 19:14:44 +0530

    Removed unnecessary config

--- adstxt/app_config.py
+++ adstxt/app_config.py
@@ -5,7 +5,5 @@ rabbitmq_username = "adstxt"
 rabbitmq_password = ""
 rabbitmq_vhost = "adstxtcrawler"
 rabbitmq_port = 5672
-send_task_log_file = currentDir + "/rabbitmq_tasks/send_task.log"
-receive_task_log_file = currentDir + "/rabbitmq_tasks/receive_task.log"
 user_download_path = currentDir + "/userdownloads/"
 crawler_download_path = currentDir + "/csv/"
